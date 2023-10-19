5d80bfbeaaad4d0c37fafaee1c74bb9c54280b3c
rajavelyu <144324653+rajavelyu@users.noreply.github.com>
2023-10-19 12:55:32 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,10 +1,19 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-
-number = int(input("Enter a value : "))
-res = fact_rec(number)
-
-print("The factorial of {} is {}.".format(number,res))
+def linear_Search(list1, n, key):  
+  
+    # Searching list1 sequentially  
+    for i in range(0, n):  
+        if (list1[i] == key):  
+            return i  
+    return -1  
+  
+  
+list1 = [1 ,3, 5, 4, 7, 9]  
+key = 7  
+  
+n = len(list1)  
+res = linear_Search(list1, n, key)  
+if(res == -1):  
+    print("Element not found")  
+else:  
+    print("Element found at index: ", res)
+  
